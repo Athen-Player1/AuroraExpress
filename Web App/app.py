4292efbfcb5c18906966d7b30226bafeaa1a146c
George E-Green <112101154+Athen-Player1@users.noreply.github.com>
2023-06-21 15:30:21 +0100

    updating app.py and adding all fields to index.html

--- Web App/app.py
+++ Web App/app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    oneliner = ""
+def home():
+    oneliner = None
     if request.method == 'POST':
         customer_key = request.form.get('customer_key')
         branch = request.form.get('branch')
@@ -20,24 +18,28 @@ def index():
         arm_ip_left = request.form.get('arm_ip_left')
         ur_robot_type = request.form.get('ur_robot_type')
 
-        oneliner = "bash <(curl -Ls bit.ly/run-aurora) server_and_nuc_deploy --branch " + branch + " customer_key=\"" + customer_key + "\" product=" + product + " reinstall=" + str(reinstall)
-
-        if product == "arm_hand_e":
-            oneliner += " hand_side=" + hand_side
-
-        if hand_side == "left":
-            oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""
-            oneliner += " arm_ip_left=\"" + arm_ip_left + "\""
-        elif hand_side == "right":
-            oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""
-            oneliner += " arm_ip_right=\"" + arm_ip_right + "\""
-        else:
-            oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""
-            oneliner += " arm_ip_right=\"" + arm_ip_right + "\""
-            oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""
-            oneliner += " arm_ip_left=\"" + arm_ip_left + "\""
+        oneliner = generate_oneliner(customer_key, branch, tag, product, hand_side, reinstall, ethercat_right_arm, arm_ip_right, ethercat_left_arm, arm_ip_left, ur_robot_type)
 
     return render_template('index.html', oneliner=oneliner)
 
-if __name__ == '__main__':
+def generate_oneliner(customer_key, branch, tag, product, hand_side, reinstall, ethercat_right_arm, arm_ip_right, ethercat_left_arm, arm_ip_left, ur_robot_type):
+    oneliner = "bash <(curl -Ls bit.ly/run-aurora) server_and_nuc_deploy --branch " + branch + " customer_key=\"" + customer_key + "\" product=" + product + " reinstall=" + str(reinstall)  
+    if product == "arm_hand_e":  
+        oneliner += " hand_side=" + hand_side  
+
+    if hand_side == "left":  
+        oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""  
+        oneliner += " arm_ip_left=\"" + arm_ip_left + "\""  
+    elif hand_side == "right":  
+        oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""  
+        oneliner += " arm_ip_right=\"" + arm_ip_right + "\""  
+    else:  
+        oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""  
+        oneliner += " arm_ip_right=\"" + arm_ip_right + "\""  
+        oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""  
+        oneliner += " arm_ip_left=\"" + arm_ip_left + "\""  
+
+    return oneliner
+
+if __name__ == "__main__":
     app.run(debug=True)
