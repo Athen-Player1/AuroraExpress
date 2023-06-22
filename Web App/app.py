febfa767908fb5e7bd8679effa918b79f81c0e98
George <georgee-green@outlook.com>
2023-06-22 14:08:50 +0100

    Changes to be committed: 	modified:   Web App/app.py 	modified:   Web App/templates/index.html

--- Web App/app.py
+++ Web App/app.py
@@ -1,45 +1,43 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    oneliner = None
+def index():
     if request.method == 'POST':
-        customer_key = request.form.get('customer_key')
-        branch = request.form.get('branch')
-        tag = request.form.get('tag')
-        product = request.form.get('product')
-        hand_side = request.form.get('hand_side')
-        reinstall = request.form.get('reinstall')
-        ethercat_right_arm = request.form.get('ethercat_right_arm')
-        arm_ip_right = request.form.get('arm_ip_right')
-        ethercat_left_arm = request.form.get('ethercat_left_arm')
-        arm_ip_left = request.form.get('arm_ip_left')
-        ur_robot_type = request.form.get('ur_robot_type')
+        customer_key = request.form['customer_key']
+        branch = request.form['branch']
+        tag = request.form['tag']
+        product = request.form['product']
+        hand_side = request.form['hand_side']
+        reinstall = request.form.get('reinstall') is not None
+        ethercat_right_arm = request.form['ethercat_right_arm']
+        arm_ip_right = request.form['arm_ip_right']
+        ethercat_left_arm = request.form['ethercat_left_arm']
+        arm_ip_left = request.form['arm_ip_left']
+        ur_robot_type = request.form['ur_robot_type']
 
-        oneliner = generate_oneliner(customer_key, branch, tag, product, hand_side, reinstall, ethercat_right_arm, arm_ip_right, ethercat_left_arm, arm_ip_left, ur_robot_type)
+        oneliner = "bash <(curl -Ls bit.ly/run-aurora) server_and_nuc_deploy --branch " + branch + " customer_key=\"" + customer_key + "\" product=" + product + " reinstall=" + str(reinstall)
 
-    return render_template('index.html', oneliner=oneliner)
+        if product == "arm_hand_e":
+            oneliner += " hand_side=" + hand_side
 
-def generate_oneliner(customer_key, branch, tag, product, hand_side, reinstall, ethercat_right_arm, arm_ip_right, ethercat_left_arm, arm_ip_left, ur_robot_type):
-    oneliner = "bash <(curl -Ls bit.ly/run-aurora) server_and_nuc_deploy --branch " + branch + " customer_key=\"" + customer_key + "\" product=" + product + " reinstall=" + str(reinstall)  
-    if product == "arm_hand_e":  
-        oneliner += " hand_side=" + hand_side  
+        if hand_side == "left":
+            oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""
+            oneliner += " arm_ip_left=\"" + arm_ip_left + "\""
+        elif hand_side == "right":
+            oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""
+            oneliner += " arm_ip_right=\"" + arm_ip_right + "\""
+        else:
+            oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""
+            oneliner += " arm_ip_right=\"" + arm_ip_right + "\""
+            oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""
+            oneliner += " arm_ip_left=\"" + arm_ip_left + "\""
 
-    if hand_side == "left":  
-        oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""  
-        oneliner += " arm_ip_left=\"" + arm_ip_left + "\""  
-    elif hand_side == "right":  
-        oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""  
-        oneliner += " arm_ip_right=\"" + arm_ip_right + "\""  
-    else:  
-        oneliner += " ethercat_right_arm=\"" + ethercat_right_arm + "\""  
-        oneliner += " arm_ip_right=\"" + arm_ip_right + "\""  
-        oneliner += " ethercat_left_arm=\"" + ethercat_left_arm + "\""  
-        oneliner += " arm_ip_left=\"" + arm_ip_left + "\""  
+        return render_template('index.html', oneliner=oneliner)
 
-    return oneliner
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
